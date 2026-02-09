21574e33a0884ecd401f353c92b8a5657285d63d
MarcossYoung <marcosyoung98@gmail.com>
2026-02-09 15:12:21 -0300

    fixed n8n ChatBot

--- frontEnd/muebles_workflow/src/components/chatBot.jsx
+++ frontEnd/muebles_workflow/src/components/chatBot.jsx
@@ -4,7 +4,7 @@ import {useLocation} from 'react-router-dom';
 import '../css/styles.css';
 
 const N8N_WEBHOOK_URL =
-	'https://primary-production-340f.up.railway.app/webhook-test/chat';
+	'https://primary-production-e8647.up.railway.app/webhook-test/chat';
 
 const Chatbot = () => {
 	const location = useLocation();
