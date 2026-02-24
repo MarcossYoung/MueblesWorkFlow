9ae718ef9ca872ce612b0e1bd8c0b950ee06e93d
Marcos Young <marcosyoung98@gmail.com>
2026-02-24 14:35:57 -0300

    Changed the sorting

--- backEnd/src/main/java/com/example/demo/controller/ProductController.java
+++ backEnd/src/main/java/com/example/demo/controller/ProductController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -45,7 +46,7 @@ public class ProductController {
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size
     ) {
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "startDate"));
         Page<ProductResponse> products = productService.getAll(pageable);
         return ResponseEntity.ok(products);
     }
